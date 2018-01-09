d66b12ccf32d054e31839f7d655d2f1d9b594767
NowhereToRun <zihao5@staff.sina.com.cn>
2018-01-09 15:26:30 +0800

    添加scope控制

--- sina/nextPage/app.js
+++ sina/nextPage/app.js
@@ -13,7 +13,9 @@
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register('./service-worker.js', './')
+      .register('./service-worker.js', {
+        scope: './'
+      })
       .then(function() {
         console.log('Service Worker Registered');
       })
