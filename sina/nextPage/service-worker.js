bce08b9f55081a1dd02e14a75b527e0824ca3d40
NowhereToRun <zihao5@staff.sina.com.cn>
2018-01-09 15:05:27 +0800

    处理子页面 父页面

--- sina/nextPage/service-worker.js
+++ sina/nextPage/service-worker.js
@@ -25,21 +25,21 @@ var filesToCache = [
   '../images/bg.jpg'
 ];
 
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
+    caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key) {
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
         if (key !== cacheName && key !== dataCacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
@@ -50,22 +50,12 @@ self.addEventListener('activate', function (e) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function (e) {
-  // console.log('[Service Worker] Fetch', e.request.url);
-  var preUrl = '/nextPage/detail-';
-  if (e.request.url.indexOf(preUrl) > -1) {
-    // 提前加载的文章    
-    e.respondWith(
-      caches.match(e.request).then(function (response) {
-        return response || fetch(e.request);
-      })
-    );
-  } else {
-    // 这个地方必须优化，否则需要检测的资源太多，fetch规则需要处理
-    // e.respondWith(
-    //   caches.match(e.request).then(function (response) {
-    //     return response || fetch(e.request);
-    //   })
-    // );
-  }
+self.addEventListener('fetch', function(e) {
+  // 线上不能拦截所有请求
+  console.log('[Service Worker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
