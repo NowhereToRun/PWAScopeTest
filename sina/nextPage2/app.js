92445b8b493b6281fb62f83867d9be310a97119f
NowhereToRun <zihao5@staff.sina.com.cn>
2018-01-10 10:41:33 +0800

    添加子域名2

---
+++ sina/nextPage2/app.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+  function showNotification() {
+    Notification.requestPermission(function(result) {
+      if (result === 'granted') {
+        navigator.serviceWorker.ready.then(function(registration) {
+          console.log('success');
+        });
+      }
+    });
+  }
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('./service-worker.js', {
+        scope: '../'
+      })
+      .then(function() {
+        console.log('Service Worker Registered');
+      })
+      .catch(function(err) {
+        console.log('Service Worker Failed. ', err);
+      });
+    showNotification();
+  }
+})();
---
+++ sina/nextPage2/service-worker.js
@@ -0,0 +1,61 @@
+// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var dataCacheName = 'sinaHome-next-page-v1';
+var cacheName = 'sinaHome-static-v1';
+var filesToCache = [
+  './',
+  './index.html',
+  './app.js',
+  '../css/homev8.min.css',
+  '../images/try_57x57_v1.png',
+  '../images/try_114x114_v1.png',
+  '../images/try_144x144_v1.png',
+  '../images/bg.jpg'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  // 线上不能拦截所有请求
+  console.log('[Service Worker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
